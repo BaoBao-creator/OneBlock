a4f13277b7623629b1870697f47b4ecdc5ce917e
BaoBao-creator <ba0ba0113377@gmail.com>
2025-09-02 19:58:42 +0700

    Update run.lua

--- run.lua
+++ run.lua
@@ -4,6 +4,8 @@ local players = game:GetService("Players")
 -- Data
 local localplayer = players.LocalPlayer
 local playername = localplayer.Name
+-- Connections
+local workspaceconnection = false
 local function mine(toolname, block)
   local blockinfo = block.BlockInfo
   local maxhealth = blockinfo.MaxHealth
@@ -13,4 +15,29 @@ local function loot(drop)
   local uuid = drop:GetAttribute("UUID")
   replicatedstorage.RequestLootPickup:InvokeServer(uuid)
   replicatedstorage.LootDestroyed:FireServer(uuid)
+end
+local function automine(v)
+    collectingFairy = v
+    if collectingFairy then
+        for _, obj in ipairs(workspace:GetChildren()) do
+            if obj:IsA("Model") and tonumber(obj.Name) then
+                collectFairy(obj)
+            end
+        end
+        fairyConnection = workspace.ChildAdded:Connect(function(obj)
+            if obj:IsA("Model") and tonumber(obj.Name) then
+                local prompt = obj:WaitForChild("ProximityPrompt", 5)
+                if prompt and collectingFairy then
+                    fireproximityprompt(prompt)
+                end
+            end
+        end)
+    else
+        if fairyConnection then
+            fairyConnection:Disconnect()
+            fairyConnection = nil
+        end
+    end
+end
+local function autoloot()
 end
