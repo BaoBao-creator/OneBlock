8536d40f8879e952bc5977fde7c1bf67099c851b
BaoBao-creator <ba0ba0113377@gmail.com>
2025-09-02 20:41:47 +0700

    Update run.lua

--- run.lua
+++ run.lua
@@ -2,25 +2,29 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
 local Workspace = game:GetService("Workspace")
-
 -- Data
 local LocalPlayer = Players.LocalPlayer
 local PlayerName = LocalPlayer.Name
-
+local toolname = nil
 -- Toggles
 local mining = false
 local looting = false
-local toolName = ""
-
 -- Connections
-local workspaceConnection
-
+local workspaceconnection
 -- Functions
-local function mine(toolName, block)
+local function isblock(obj)
+    if obj:IsA("Model") and obj:FindFirstChild("BlockInfo") then return true end
+    return false
+end
+local function isdrop(obj)
+    if obj:IsA("Model") and obj:FindFirstChild("INFO") and obj:GetAttribute("UUID") then return true end
+    return false
+end
+local function mine(block)
+    if not toolname then return end
     local blockInfo = block.BlockInfo
     local maxHealth = blockInfo.MaxHealth
-
-    Workspace[PlayerName][toolName].ToolHit:FireServer({{
+    Workspace[PlayerName][toolname].ToolHit:FireServer({{
         ["healthValue"]    = blockInfo.Health,
         ["blockInfoFolder"] = blockInfo,
         ["position"]        = block.WorldPivot.Position,
@@ -30,47 +34,43 @@ local function mine(toolName, block)
         ["healthBarGui"]    = block.HealthBar,
         ["hardness"]        = blockInfo.BlockHardness.Value,
         ["health"]          = blockInfo.Health.Value
-    }}, toolName)
+    }}, toolname)
 end
-
 local function loot(drop)
     local uuid = drop:GetAttribute("UUID")
     ReplicatedStorage.RequestLootPickup:InvokeServer(uuid)
     ReplicatedStorage.LootDestroyed:FireServer(uuid)
 end
-
-local function updateConnection()
-    if (mining or looting) and not workspaceConnection then
-        workspaceConnection = Workspace.ChildAdded:Connect(function(obj)
-            if mining and obj:IsA("Model") and obj:FindFirstChild("BlockInfo") then
-                mine(toolName, obj)
-            elseif looting and obj:IsA("Model") and obj:FindFirstChild("Info") then
+local function updateconnection()
+    if (mining or looting) and not workspaceconnection then
+        workspaceconnection = Workspace.ChildAdded:Connect(function(obj)
+            if mining and isblock(obj) then
+                mine(obj)
+            elseif looting and isdrop(obj) then
                 loot(obj)
             end
         end)
-    elseif not mining and not looting and workspaceConnection then
-        workspaceConnection:Disconnect()
-        workspaceConnection = nil
+    elseif not mining and not looting and workspaceconnection then
+        workspaceconnection:Disconnect()
+        workspaceconnection = nil
     end
 end
-
-local function autoMine(state)
+local function automine(state)
     mining = state
     if mining then
         for _, obj in ipairs(Workspace:GetChildren()) do
-            if obj:IsA("Model") and obj:FindFirstChild("BlockInfo") then
-                mine(toolName, obj)
+            if isblock(obj) then
+                mine(obj)
             end
         end
     end
     updateConnection()
 end
-
 local function autoLoot(state)
     looting = state
     if looting then
         for _, drop in ipairs(Workspace:GetChildren()) do
-            if drop:IsA("Model") and drop:FindFirstChild("Info") and drop:GetAttribute("UUID") then
+            if isdrop(drop) then
                 loot(drop)
             end
         end
